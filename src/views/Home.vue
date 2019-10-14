1a101b4680c572a17d49d0e2c3e613b3bd80dea3
seajensen <seajensen@gmail.com>
2019-10-14 15:16:11 -0600

    removed SASS, recreated project

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,16 @@
 <template>
-  <HelloWorld />
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from '../components/HelloWorld'
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
+  name: 'home',
   components: {
     HelloWorld
   }
