23af90cf2078043660c124391ff090fca7f13453
seajensen <seajensen@gmail.com>
2019-11-16 13:42:07 -0700

    Working on letting images show up

--- src/components/TeamBox.vue
+++ src/components/TeamBox.vue
@@ -2,7 +2,26 @@
 <div id="dex-box">
     <p>Click on a Pokémon to Learn More</p><br>
     <div id="TeamDisplay">
-        <p>Name: {{ name[0] }}, {{ name[1] }}, {{ name[2] }}, {{ name[3] }}</p>
+        <p>Name: {{ name[0] }}, {{ height[0] }}, {{ weight[0] }}, {{ sprite[0] }}, {{ length }}</p>
+    </div>
+    <div style="display: flex;">
+        <img :src="sprite[0]">
+        <v-btn style="background-color: red; color: white; margin: auto; width: 120px;"
+        @click="infoCheck()"
+        >{{ name[0] }}</v-btn>
+    </div>
+    <div style="background-color: rgb(221, 221, 221);" v-if="infoToggle">
+        <p>Type(s): {{ type[0] }}</p>
+        <v-divider></v-divider>
+        <p>Height: {{ height[0] }}</p>
+        <v-divider></v-divider>
+        <p>Weight: {{ weight[0] }}</p>
+        <v-divider></v-divider>
+        <v-btn style="background-color: red; color: white; margin: auto; width: 90%; margin-top: 10px; margin-bottom: 10px;"
+        @click="infoCheck()"
+        >
+            Remove from Team
+        </v-btn>
     </div>
 </div>
 </template>
@@ -12,9 +31,37 @@
 import PkComp from './PkComp';
 
 export default {
+  data: () => ({
+    infoToggle: false,
+  }),
   computed: {
+      type() {
+          return this.$store.state.teamType;
+      },
+      height() {
+          return this.$store.state.teamHeight;
+      },
+      weight() {
+          return this.$store.state.teamWeight;
+      },
       name() {
           return this.$store.state.teamName;
+      },
+      sprite() {
+          return this.$store.state.teamSprite;
+      },
+      length() {
+          return this.$store.state.teamName.length;
+      }
+
+  },
+  methods: {
+      infoCheck() {
+          if(this.infoToggle == false) {
+            this.infoToggle = true;
+          } else {
+            this.infoToggle = false;
+          }
       }
   }
 };
