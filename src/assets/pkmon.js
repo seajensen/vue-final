c53feaec910fbb859ec1f1a73bf65af6482e61a8
seajensen <seajensen@gmail.com>
2019-11-23 13:22:00 -0700

    Updating pkmon.js

--- src/assets/pkmon.js
+++ src/assets/pkmon.js
@@ -148,6 +148,38 @@ export const pkmon = [
         weight: 190
     },
     {
+        id: 6,
+        name: "charizard",
+        sprites: {
+            back_default: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/6.png",
+            back_female: null,
+            back_shiny: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/shiny/6.png",
+            back_shiny_female: null,
+            front_default: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png",
+            front_female: null,
+            front_shiny: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/6.png",
+            front_shiny_female: null
+        },
+        types: [
+        {
+        slot: 2,
+        type: {
+        name: "flying",
+        url: "https://pokeapi.co/api/v2/type/3/"
+        }
+        },
+        {
+        slot: 1,
+        type: {
+        name: "fire",
+        url: "https://pokeapi.co/api/v2/type/10/"
+        }
+        }
+        ],
+        height: 17,
+        weight: 905
+    },
+    {
         name: "squirtle",
         sprites: {
             back_default: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/7.png",
